101d223878ad940bbda94414b7b29f289b40c43f
Rafael Timermans Díez <rafatimer@hotmail.com>
2021-03-23 20:32:42 +0100

    añado el cargar librerias

---
+++ kbmR_afa-prj.R
@@ -0,0 +1,63 @@
+###   ###
+## kbmR ##
+###   ###
+
+# source("kbmR-prj.R")
+
+
+  rm(list=ls(all=TRUE))
+
+library( gmp) # bigz
+#library( gat)
+library( lattice)
+library( foreach)
+library( cluster)
+
+#### module ####
+setwd("./defs")
+source("kbmr-definition.R")
+setwd("..")
+
+#### module ####
+setwd("./tabs")
+source("kbmr-inout.R")
+#source("kbmr-spectrum.R")
+setwd("..")
+
+#### module ####
+setwd("./optalg")
+source("kbmr-optimization.R")
+setwd("..")
+
+#### module ####
+setwd("./query")
+#source("kbmr-queries.R")
+#source("kbmr-explanation.R")
+setwd("..")
+
+#### module RAFAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA ####
+setwd("./Rafa")
+source("custom.base.R")
+source("vector.swap.R")
+source("combinaciones.base.R")
+source("getBestSwap.R")
+source("vns.rafa.R")
+setwd("..")
+
+### models, kbmr.demo()
+setwd("./models")
+cat("Models source reading:\n")
+#source("kbmr-demo1.R")
+#source("kbmr-demo2.R")
+
+#source("kbmr-examples1.R")
+#source("kbmr-examples2.R")
+#source("kbmr-examples3.R")
+#source("kbmr-examples4.R")
+#source("kbmr-examples5.R")
+#source("kbmr-examples6.R")
+#source("kbmr-examples7.R")
+
+cat("Models source OK.\n")
+setwd("..")
+  
